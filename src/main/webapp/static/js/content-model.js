5ab30cbe8dc97b816dfbc275e921bae2bf49bda2
Viddu Devigere <vdevigere@gmail.com>
2014-08-11 20:00:20 -0700

    Wrapping contentview and model in a module

--- src/main/webapp/static/js/content-model.js
+++ src/main/webapp/static/js/content-model.js
@@ -1,59 +1,61 @@
-var app = app || {};
+var contentModule = (function(module){
+	Content = Backbone.Model.extend({});
 
-app.Content = Backbone.Model.extend({});
+	ContentCollection = Backbone.Collection.extend({
+		model : Content,
+		activeOnly : false,
+		tags : [],
 
-app.ContentCollection = Backbone.Collection.extend({
-	model : app.Content,
-	activeOnly : false,
-	tags : [],
+		getQueryString : function() {
+			var qs = "";
+			if (this.activeOnly) {
+				qs += 'activeOnly=' + this.activeOnly + '&';
+			}
+			for (tag in this.tags) {
+				qs += 'tags=' + this.tags[tag] + '&'
+			}
+			if (qs) {
+				return '?' + qs;
+			} else {
+				return qs;
+			}
+		},
 
-	getQueryString : function() {
-		var qs = "";
-		if (this.activeOnly) {
-			qs += 'activeOnly=' + this.activeOnly + '&';
+		url : function() {
+			var url = '/targeted-content/api/content';
+			fullURL = url + this.getQueryString();
+			console.log('Fetch URL=' + fullURL);
+			return fullURL;
+		},
+
+		findActive : function() {
+			this.activeOnly = true;
+			this.fetch({
+				reset : true
+			});
+			return this;
+		},
+
+		findByTag : function(csvTagList) {
+			if (csvTagList) {
+				this.tags = csvTagList.split(',');
+			} else {
+				this.tags = [];
+			}
+			this.fetch({
+				reset : true
+			});
+			return this;
+		},
+
+		findAll : function() {
+			this.activeOnly = false;
+			this.fetch({
+				reset : true
+			});
+			return this;
 		}
-		for (tag in this.tags) {
-			qs += 'tags=' + this.tags[tag] + '&'
-		}
-		if (qs) {
-			return '?' + qs;
-		} else {
-			return qs;
-		}
-	},
+	});
 
-	url : function() {
-		var url = '/targeted-content/api/content';
-		fullURL = url + this.getQueryString();
-		console.log('Fetch URL=' + fullURL);
-		return fullURL;
-	},
-
-	findActive : function() {
-		this.activeOnly = true;
-		this.fetch({
-			reset : true
-		});
-		return this;
-	},
-
-	findByTag : function(csvTagList) {
-		if (csvTagList) {
-			this.tags = csvTagList.split(',');
-		} else {
-			this.tags = [];
-		}
-		this.fetch({
-			reset : true
-		});
-		return this;
-	},
-
-	findAll : function() {
-		this.activeOnly = false;
-		this.fetch({
-			reset : true
-		});
-		return this;
-	}
-});
+	return module;
+})(contentModule || {});
--- src/main/webapp/static/js/content-view.js
+++ src/main/webapp/static/js/content-view.js
@@ -1,66 +1,80 @@
-var app = app || {};
+var contentModule = (function(module) {
+	ContentDetailView = Backbone.View.extend({
+		template : Handlebars.compile($('#content-row-template').html()),
 
-// The Content Detail View
-app.ContentDetailView = Backbone.View.extend({
-	template : Handlebars.compile($('#content-row-template').html()),
-
-	render : function() {
-		var html = this.template(this.model.attributes);
-		this.$el.append(html);
-		return this;
-	}
-});
-
-// The Content List Table
-app.ContentListView = Backbone.View.extend({
-	el : $("#searchResultsForm"),
-
-	template : Handlebars.compile($('#content-container-template').html()),
-
-	events : {
-		'click #validOnly' : 'toggleValidContent', // Valid Only Checkbox
-		'change .tags' : 'tagChanged',
-	},
-
-	initialize : function(initialContent) {
-		this.collection = new app.ContentCollection(initialContent);
-
-		// Listen to "reset" events on model and render view if model is reset.
-		this.listenTo(this.collection, "reset", this.render);
-
-		// Render on initialize
-		this.render();
-	},
-
-	render : function() {
-		// Render the Headers
-		var header = this.template();
-		var contentContainer = this.$el.find('.contentContainer');
-		contentContainer.html(header);
-
-		// Render each row of collection.
-		this.collection.each(function(contentData) {
-			var contentView = new app.ContentDetailView({
-				el : this.$el.find('tbody'),
-				model : contentData
-			});
-			contentView.render();
-		}, this);
-	},
-
-	toggleValidContent : function(e) {
-		if (e.currentTarget.checked) {
-			this.collection.findActive();
-		} else {
-			this.collection.findAll();
+		render : function() {
+			var html = this.template(this.model.attributes);
+			this.$el.append(html);
+			return this;
 		}
-	},
+	});
 
-	tagChanged : function(e){
-		this.collection.findByTag(e.currentTarget.value);
-	},
-});
+	// The Content List Table
+	ContentListView = Backbone.View.extend({
+		el : $("#searchResultsForm"),
 
-new app.ContentCollection().fetch().done(function(data) {
-	new app.ContentListView(data);
-});
+		template : Handlebars.compile($('#content-container-template').html()),
+
+		events : {
+			'click #validOnly' : 'toggleValidContent', // Valid Only Checkbox
+			'change .tags' : 'tagChanged',
+		},
+
+		initialize : function(initialContent) {
+			this.collection = new ContentCollection(initialContent);
+
+			// Listen to "reset" events on model and render view if model is
+			// reset.
+			this.listenTo(this.collection, "reset", this.render);
+
+			// Render on initialize
+			this.render();
+		},
+
+		render : function() {
+			// Render the Headers
+			var header = this.template();
+			var contentContainer = this.$el.find('.contentContainer');
+			contentContainer.html(header);
+
+			// Render each row of collection.
+			this.collection.each(function(contentData) {
+				var contentView = new ContentDetailView({
+					el : this.$el.find('tbody'),
+					model : contentData
+				});
+				contentView.render();
+			}, this);
+		},
+
+		toggleValidContent : function(e) {
+			if (e.currentTarget.checked) {
+				this.collection.findActive();
+			} else {
+				this.collection.findAll();
+			}
+		},
+
+		tagChanged : function(e) {
+			this.collection.findByTag(e.currentTarget.value);
+		},
+	});
+
+	var contentListView;
+	// public
+	module.loadData = function() {
+		new ContentCollection().fetch().done(function(data) {
+			contentListView = new ContentListView(data);
+		});
+	}
+
+	module.getCollection = function(){
+		return contentListView.collection;
+	}
+
+	return module;
+})(contentModule || {});
+
+if ($('#searchResultsForm').length){
+	contentModule.loadData();
+}
