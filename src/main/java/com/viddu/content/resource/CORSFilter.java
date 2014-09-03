73ccb630364ada080d5141fbad816fc974fe5f65
Viddu Devigere <vdevigere@gmail.com>
2014-09-03 15:49:49 -0700

    Adding more CORS headers

--- src/main/java/com/viddu/content/resource/CORSFilter.java
+++ src/main/java/com/viddu/content/resource/CORSFilter.java
@@ -11,13 +11,19 @@ import org.slf4j.LoggerFactory;
 
 public class CORSFilter implements ContainerResponseFilter {
 
+    private static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    private static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    private static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
     private static final Logger logger = LoggerFactory.getLogger(CORSFilter.class);
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
             throws IOException {
         logger.debug("Adding CORS headers on response");
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        logger.debug("Headers={}", requestContext.getHeaders());
+        responseContext.getHeaders().add(ACCESS_CONTROL_ALLOW_ORIGIN,"*");
+        responseContext.getHeaders().add(ACCESS_CONTROL_ALLOW_METHODS, "POST, GET, OPTIONS");
+        responseContext.getHeaders().add(ACCESS_CONTROL_ALLOW_HEADERS, "Origin, X-Requested-With, Content-Type, Accept");
     }
 
 }
