100151b793400f3966a0b8f52aa9e1fc8a275443
Viddu Devigere <vdevigere@gmail.com>
2014-09-29 21:48:42 -0700

    Adding logic to create an index if none present

--- src/main/java/com/viddu/content/elasticsearch/ElasticSearchConnectionManager.java
+++ src/main/java/com/viddu/content/elasticsearch/ElasticSearchConnectionManager.java
@@ -8,6 +8,8 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
+import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.node.Node;
 import org.slf4j.Logger;
@@ -30,8 +32,24 @@ public class ElasticSearchConnectionManager implements ServletContextListener {
         logger.debug("Starting ElasticSearch node..");
         node = nodeBuilder().node();
         Client client = node.client();
+
+        String indexName = config.getString("INDEX_NAME");
+        String contentType = config.getString("TYPE_NAME");
+        createIndexIfNoneExists(client, indexName, contentType);
+
         ServletContext context = sce.getServletContext();
         context.setAttribute(config.getString(CLIENT_NAME), client);
+    }
+
+    private void createIndexIfNoneExists(Client client, String indexName, String contentType) {
+        final IndicesExistsResponse res = client.admin().indices().prepareExists(indexName).execute().actionGet();
+        if (!res.isExists()) {
+            final CreateIndexRequestBuilder createIndexRequestBuilder = client.admin().indices()
+                    .prepareCreate(indexName);
+            String mapping = "{\"content\":{\"properties\":{\"contentDataSet\":{\"properties\":{\"data\":{\"type\":\"string\"},\"weight\":{\"type\":\"long\"}}},\"endDate\":{\"type\":\"date\",\"format\":\"dateOptionalTime\"},\"id\":{\"type\":\"string\"},\"name\":{\"type\":\"string\"},\"startDate\":{\"type\":\"date\",\"format\":\"dateOptionalTime\"},\"target\":{\"properties\":{\"tags\":{\"type\":\"string\",\"index\":\"not_analyzed\"}}}}}}";
+            createIndexRequestBuilder.addMapping(contentType, mapping);
+            createIndexRequestBuilder.execute().actionGet();
+        }
     }
 
     @Override
