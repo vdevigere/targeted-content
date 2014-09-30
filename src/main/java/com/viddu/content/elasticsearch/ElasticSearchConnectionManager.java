55958afdb950ff6a43469537e603563dee642a9d
Viddu Devigere <vdevigere@gmail.com>
2014-09-29 22:59:44 -0700

    Executing the index creation in a future to wait for the async exists call to return

--- src/main/java/com/viddu/content/elasticsearch/ElasticSearchConnectionManager.java
+++ src/main/java/com/viddu/content/elasticsearch/ElasticSearchConnectionManager.java
@@ -2,12 +2,17 @@ package com.viddu.content.elasticsearch;
 
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequestBuilder;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
 import org.elasticsearch.client.Client;
@@ -32,18 +37,26 @@ public class ElasticSearchConnectionManager implements ServletContextListener {
         logger.debug("Starting ElasticSearch node..");
         node = nodeBuilder().node();
         Client client = node.client();
-
-        String indexName = config.getString("INDEX_NAME");
-        String contentType = config.getString("TYPE_NAME");
-        createIndexIfNoneExists(client, indexName, contentType);
-
         ServletContext context = sce.getServletContext();
         context.setAttribute(config.getString(CLIENT_NAME), client);
+        try {
+            createIndexIfNoneExists(client);
+        } catch (ElasticsearchException | InterruptedException | ExecutionException e) {
+            logger.error("Exception: ", e);
+        }
     }
 
-    private void createIndexIfNoneExists(Client client, String indexName, String contentType) {
-        final IndicesExistsResponse res = client.admin().indices().prepareExists(indexName).execute().actionGet();
-        if (!res.isExists()) {
+    private void createIndexIfNoneExists(Client client) throws ElasticsearchException, InterruptedException,
+            ExecutionException {
+        String indexName = config.getString("INDEX_NAME");
+        String contentType = config.getString("TYPE_NAME");
+        FutureTask<IndicesExistsResponse> future = new FutureTask<IndicesExistsResponse>(() -> {
+            final IndicesExistsResponse res = client.admin().indices().prepareExists(indexName).execute().actionGet();
+            return res;
+        });
+        Executors.newSingleThreadExecutor().execute(future);
+
+        if (!future.get().isExists()) {
             final CreateIndexRequestBuilder createIndexRequestBuilder = client.admin().indices()
                     .prepareCreate(indexName);
             String mapping = "{\"content\":{\"properties\":{\"contentDataSet\":{\"properties\":{\"data\":{\"type\":\"string\"},\"weight\":{\"type\":\"long\"}}},\"endDate\":{\"type\":\"date\",\"format\":\"dateOptionalTime\"},\"id\":{\"type\":\"string\"},\"name\":{\"type\":\"string\"},\"startDate\":{\"type\":\"date\",\"format\":\"dateOptionalTime\"},\"target\":{\"properties\":{\"tags\":{\"type\":\"string\",\"index\":\"not_analyzed\"}}}}}}";
